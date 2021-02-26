47c96f8a4bee2895842b08c7c1854e59407e0fa7
H.Mollov <hasan.mollov93@gmail.com>
2021-02-26 19:30:39 +0200

    Header design implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { UserPanelComponent } from './core/user-panel/user-panel.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavigationComponent,
+    UserPanelComponent,
+    FooterComponent
+  ]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, UserPanelComponent],
+  declarations: [
+    NavigationComponent,
+    FooterComponent,
+    UserPanelComponent
+  ],
   imports: [
     CommonModule
   ]
