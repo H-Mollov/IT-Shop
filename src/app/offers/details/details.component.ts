5c824fd3a6a470f7ae8f0fd73ca5b9d79d1912e5
H.Mollov <hasan.mollov93@gmail.com>
2021-03-16 20:26:42 +0200

    Implemented Like, Update and Delete functionality to offers.

--- src/app/offers/details/details.component.ts
+++ src/app/offers/details/details.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { offers } from '../../+store/actions';
+import { offers, login } from '../../+store/actions';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnDestroy {
 
   constructor(
     private activatdRoute: ActivatedRoute,
@@ -17,13 +17,25 @@ export class DetailsComponent implements OnInit {
 
   id = this.activatdRoute.snapshot.params.id;
   currentElement;
+  currentOwner: string;
+  isLiked: boolean = false;
 
   findElement = this.store.select(offers).subscribe((data) => {
     this.currentElement = data.offers.currentOffers.results.find(element => element.objectId === this.id);
     console.log(this.currentElement);
   })
 
-  ngOnInit(): void {
-  }
+  getOwner = this.store.select(login).subscribe((data) => {
+    this.currentOwner = data.login.currentUser.userId;
+    if (this.currentOwner !== this.currentElement.owner) {
+      if (this.currentElement.likes.includes(this.currentOwner)) {
+        this.isLiked = true;
+      }
+    }
+  })
 
+  ngOnDestroy(): void {
+    this.findElement.unsubscribe();
+    this.getOwner.unsubscribe();
+  }
 }
