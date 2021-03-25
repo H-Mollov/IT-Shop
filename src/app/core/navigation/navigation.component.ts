3cf5b5e6cf81bc80e17384502fc062556e288216
H.Mollov <hasan.mollov93@gmail.com>
2021-03-25 23:39:16 +0200

    Implemented hamburger menu for mobile devices

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { OffersService } from 'src/app/offers/offers.service';
 
@@ -7,17 +8,42 @@ import { OffersService } from 'src/app/offers/offers.service';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnChanges {
 
   constructor(
     private offer: OffersService,
     private store: Store<any>,
-  ) { }
+    private router: Router
+  ) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.menuButtonIsToggled = false;
+        this.menuIsHidden = false;
+      }
+    })
+  }
 
   isLogged;
+  menuButtonIsToggled: boolean = false;
+  menuIsHidden: boolean = false;
+
+  toggleMenu() {
+    if (this.menuButtonIsToggled) {
+      this.menuButtonIsToggled = false;
+      this.menuIsHidden = false;
+    } else {
+      this.menuButtonIsToggled = true
+      this.menuIsHidden = true;
+    }
+  }
+
   currentUser = this.store.subscribe((data) => {
     this.isLogged = data.login.Session;
   })
 
   offerService = this.offer;
+
+  ngOnChanges(): void {
+
+  }
 }
